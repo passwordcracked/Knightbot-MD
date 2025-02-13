e3ce9d26d069b9c32ef847928a064c27b7db251c
passwordcracked <make.automatation@gmail.com>
2025-02-14 02:34:42 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Management Bot.",
+  botOwner: 'Kato Seth', // Your name
+  ownerNumber: '256746883065', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
